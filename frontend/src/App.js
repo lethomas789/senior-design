e7df5c6985c3be9ad58e8bbe0a08825c1f47d481
Thomas Le <thele@ucdavis.edu>
2019-02-04 14:06:46 -0800

    Init react frontend
    Used npx create-react-app. Not sure if breaks with other package.json

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        HELLO
+        <Button variant="contained" color="primary">
+          HELLO WORLD
+        </Button>
+      </div>
+    );
+  }
+}
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export default App;
