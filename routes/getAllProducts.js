a3c9ad645ec8f00056a9f569fa3fdc62a7abe9dc
Thomas Le <thele@ucdavis.edu>
2019-02-09 22:14:51 -0800

    Add route getAllProducts

---
+++ routes/getAllProducts.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+router.get('/', (req, res) => {
+  let productsRef = db.collection('products');
+
+  productsRef.get()
+    .then(snapshot => {
+      let products = [];
+      snapshot.forEach(doc => {
+        // push each product doc to array
+        products.push(doc.data());
+      });
+
+      return res.status(200).json({
+        success: true,
+        data: products
+      });
+    })
+    .catch(err => {
+      console.log('Error in getting products:', err);
+      res.status(400).json({
+        success: false,
+        message: 'Error in getting proudcts'
+      });
+    });
+})
+
+module.exports = router;
