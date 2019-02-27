df9038a5ef054d9b280fcbb1c9912df1f91c0f38
Thomas Le <thele@ucdavis.edu>
2019-02-27 14:01:36 -0800

    Update adminProducts

--- routes/adminProducts.js
+++ routes/adminProducts.js
@@ -5,9 +5,16 @@ const admin = require('firebase-admin');
 const db = admin.firestore();
 
 
+/**
+ * Adds new product for vendor.
+ */
 router.post('/addNewProduct', (req, res) => {
+  var size = '';  // not all products will have size
+  // TODO figure out categories
+
+
   if (req.body.params){ 
-    var vendor = req.body.params.vendor;
+    var vid = req.body.params.vid;
     var user = req.body.params.user;
     var productInfo = req.body.params.productInfo;
     var productName = req.body.params.productName;
@@ -17,7 +24,7 @@ router.post('/addNewProduct', (req, res) => {
     var stock = req.body.params.stock;
   }
   else {
-    var vendor = req.body.vendor;
+    var vid = req.body.vid;
     var user = req.body.user;
     var productInfo = req.body.productInfo;
     var productName = req.body.productName;
@@ -32,8 +39,96 @@ router.post('/addNewProduct', (req, res) => {
    * 2. check if user is admin in vendor info
    * 3. add product to products root colletion
    * 4. add product to vendors/products
-   *
    */
+
+   if (!vid || !user || !productInfo || !productName || !productPrice || !stock) {
+    console.log('Error: missing params for adding new product.');
+    return res.status(400).json({
+      success: false,
+      message: 'Error: missing params for adding new product.'
+    });
+   }
+
+   // check existing vendor
+   let vendorRef = db.collection('vendors').doc(vid);
+   vendorRef.get().then(doc => {
+    if (!doc.exists) {
+      console.log('Error: no such vendor for given vid.');
+      return res.status(400).json({
+        success: false,
+        message: 'Error: no such vendor for given vid. '
+      });
+    }
+
+    // check existing admin user
+    vendorRef.collection('admins').doc(user).get().then(doc => {
+      if (!doc.exists) {
+        console.log('Error: provided user is not an admin for given vendor.');
+        return res.status(400).json({
+          success: false,
+          messaage: 'Error: provided user is not an admin for given vendor.'
+        });
+      }
+
+      let productData = {
+        productInfo: productInfo,
+        productName: productName,
+        productPrice: productPrice,
+        stock: stock,
+        purchasedStock: 0,
+        vid: vid,
+        productPicture: 'TODO'
+      };
+
+      db.collection('products').add(productData)
+      .then(ref => {
+        console.log('Added new product with ID: ', ref.id);
+
+        db.collection('products').doc(ref.id).update({pid: ref.id});
+
+        vendorRef.collection('products').doc(ref.id).set(productData)
+        .then(_ => {
+          console.log('Succesfully added new product.');
+          return res.status(200).json({
+            success: true,
+            message: 'Successfully added new product.' 
+          });
+        })
+        .catch(err => {
+          console.log('Error in adding new product:', err);
+          return res.status(500).json({
+            success: false,
+            message: 'Error in adding new product: ' + err
+          });
+        });
+
+      })
+      .catch(err => {  // catch for setting new product
+        console.log('Error in adding new product:', err);
+        return res.status(500).json({
+          success: false,
+          message: 'Error in adding new product: ' + err
+        });
+      });
+
+    })
+    .catch(err => {  // catch for adminRef
+      console.log('Error in getting adminref:', err);
+      return res.status(500).json({
+        success: false,
+        message: 'Error in getting adminRef: ' + err
+      });
+    });
+
+   })
+   .catch(err => {   // catch for vendorref.get
+     console.log('Error in getting vendorRef:', err);
+     return res.status(500).json({
+       success: false,
+       message: 'Error in getting vendorRef: ' + err
+     });
+   });
+
 });  // END POST /addNewProduct
 
 
