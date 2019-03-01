851071e608cb530afefa860c64d4c79d855092f8
Thomas Le <thele@ucdavis.edu>
2019-02-28 17:40:31 -0800

    Update TestPaypal w/ comments on testing

--- frontend/src/components/TestPaypal/TestPaypal.js
+++ frontend/src/components/TestPaypal/TestPaypal.js
@@ -8,6 +8,13 @@ export default class TestPaypal extends Component {
       console.log("Payment successful!", payment);
       // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
       // alert('Transaction completed by ' + payment.email);
+      
+      // TODO, payment contains info, send it to backend
+      // payment.payerID
+      // payment.paymentID
+
+      // payment.returnUrl can be used to route back to somewhere
+      // or route ourselves
     }
 
     const onCancel = (data) => {
@@ -25,7 +32,7 @@ export default class TestPaypal extends Component {
 
     let env = 'sandbox'; // you can set this string to 'production'
     let currency = 'USD'; // you can set this string from your props or state  
-    let total = 0.01;  // this is the total amount (based on currency) to charge
+    let total = 1.00;  // this is the total amount (based on currency) to charge
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     // TODO DO NOT CODE IN YOUR APP ID, LINK TO A FILE THAT IS GIT IGNORED
@@ -44,17 +51,25 @@ export default class TestPaypal extends Component {
     let paymentOptions = {
       "intent": "sale",
       "payer": {
-        "payment_method": "paypal"
+        "payer_info": {  // payer_info not saved to paypal transaction, will need to send email to vendor with transaction ID for cross reference
+          "email": "test@example.com",
+          "first_name": "Raymond",
+          "last_name": "Hong"
+        }
+
+        // "payment_method": "paypal"
       },
       "redirect_urls": {
-        "return_url": "localhost:3000", // send back to localhosts
-        "cancel_url": "localhost:3000/testPaypal"
+        "return_url": "www.google.com", 
+        "cancel_url": "www.reddit.com",
+        // "return_url": "http:localhost:3000", // send back to localhosts
+        // "cancel_url": "http:localhost:3000/testPaypal"
       },
       "transactions": [{
         "item_list": {
           "items": [{
-            "name": "item",
-            "sku": "item",
+            "name": "newItem with payer_info 4:31",
+            // "sku": "p0",  // stock keeping unit, 
             "price": "1.00",
             "currency": "USD",
             "quantity": 1
@@ -62,16 +77,24 @@ export default class TestPaypal extends Component {
         },
         "amount": {
           "currency": "USD",
-          "total": "0.01"
+          "total": "1.00"
         },
-        "description": "This is the payment description."
-      }]
+        "description": "This is a sale.",  // purchase description; memo for vendor
+      }],
+      "note_to_payer": "Pickup the sale at this location:"  // does a popup, not incuded in transaction on paypal
     };
 
     // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
     return (
-      <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} shipping={1}/>
-      // shipping = {1} means no shipping 
+      // shipping = {1} means no shipping
+      <div>
+        <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} shipping={1} paymentOptions={paymentOptions} />
+
+        <br></br>
+
+        <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} shipping={1} />
+      </div>
+
     );
   }
 }
