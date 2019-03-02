44106a4d3a51137f19cdd6deb6ad58ca65d0ccb7
Thomas Le <thele@ucdavis.edu>
2019-03-01 21:16:07 -0800

    Add GET getVendorOrders

--- routes/orders.js
+++ routes/orders.js
@@ -32,13 +32,13 @@ router.post('/', (req, res) => {
    * 3. Write to vendor's orders.
    */
 
-   if (!items || !totalPrice || !vid || !user || !paymentID || !payerID) {
-     console.log('Error: missing request params in orders route.');
-     return res.status(200).json({
-        success: false,
-        message: 'Error: missing request params in orders route.'
-     });
-   }
+  if (!items || !totalPrice || !vid || !user || !paymentID || !payerID) {
+    console.log('Error: missing request params in orders route.');
+    return res.status(200).json({
+      success: false,
+      message: 'Error: missing request params in orders route.'
+    });
+  }
   
   var userRef = db.collection('users').doc(user);
   userRef.get().then(doc => {
@@ -52,6 +52,10 @@ router.post('/', (req, res) => {
 
     // 7:44 test paymentID = transactionID
     // paymentID = LR47X3Y9CA5706187855000T
+    // ok so transaction id does not equal paymentID
+
+    // buyer@buyer.com
+    // "www.google.com/?paymentId=PAYID-LR5AQFQ0YN32001HC067310S&token=EC-7KS50256JG196743N&PayerID=LBVNHUDLVK75E"
 
     let date = admin.firestore.Timestamp.now();
     let orderData = {
@@ -62,7 +66,10 @@ router.post('/', (req, res) => {
       vid: vid,  // TODO vendor list
       user: user,
       date: date,
-      paid: true  // if done through paypal express checkout, then paid
+      paid: true,  // if done through paypal express checkout, then paid
+      pickedUp: false,
+      firstName: doc.data().name.firstName,
+      lastName: doc.data().name.lastName
     };
 
     // TODO: have diff route if clubs want to do cash pickup
@@ -94,6 +101,99 @@ router.post('/', (req, res) => {
   });
 
 });
+
+
+/**
+ * GET vendor's order history
+ * 
+ * @param vid - vendor vid
+ */
+router.get('/getVendorOrders', (req, res) => {
+  if (req.query.params) {
+    var vid = req.query.params.vid;
+  }
+  else {
+    var vid = req.query.vid
+  }
+
+  if (!vid) {
+    console.log('Error: missing request params in GET orders route.');
+    return res.status(200).json({
+      success: false,
+      message: 'Error: missing request params in GET orders route.'
+    });
+  }
+
+  // check to make sure given vid exists
+  let vendorsRef = db.collection('vendors').doc(vid);
+  vendorsRef.get().then(vendorDoc => {
+    if (!vendorDoc.exists) {
+      console.log('Error: provided vendor does not exist:', vid);
+      return res.status(200).json({
+        sucess: false,
+        message: 'Error: provided vid does not exist: ' + vid
+      });
+    }
+
+    // now get vendor orders
+    let orders = [];
+
+    vendorsRef.collection('orders').orderBy('date', 'desc').get().then(snapshot => {
+        snapshot.forEach(doc => {
+          let orderData = {
+            // have to call toDate on firestore data or else errors
+            date: doc.data().date.toDate(),
+            items: doc.data().items,
+            totalPrice: doc.data().totalPrice,
+            paid: doc.data().paid,
+            user: doc.data().user,
+            firstName: doc.data().firstName,
+            lastName: doc.data().lastName
+          };
+
+          // NOTE: chosen not to send payment id and payer id
+
+          // TODO, transaction ID
+          // might be too much of a pain not to use the library
+
+          orders.push(orderData);
+        });
+
+        console.log('Successfully retrieved vendor order history.');
+        return res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved vendor order history.',
+          orders: orders
+        });
+    })
+    .catch(err => {  // catch for vendor orders get
+      console.log('Server error in retrieving vendor orders:', err);
+      return res.status(200).json({
+        sucess: false,
+        message: 'Server error in retrieving vendor orders: ' + err
+      });
+    });
+
+  })
+  .catch(err => {   // catch for vendor get
+    console.log('Server error in retrieving user:', err);
+    return res.status(200).json({
+      sucess: false,
+      message: 'Server error in retrieving user: ' + err
+    });
+  });
+});
+
+/**
+ * Let admin for vendor update orders when picked up.
+ * 
+ * @param vid - vendor id
+ * @param user - vendor admin
+ */
+
+ router.patch('/updateOrder', (req, res) => {
+
+ });
 
 
 module.exports = router;
