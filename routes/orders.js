15d297dbab9cbc131dd79e5715da754a69e68c89
Thomas Le <thele@ucdavis.edu>
2019-03-02 16:12:42 -0800

    Init test emails w/ email-templates library

--- routes/orders.js
+++ routes/orders.js
@@ -4,6 +4,9 @@ const firebase = require('firebase');
 const admin = require('firebase-admin');
 const db = admin.firestore();
 const nodemailer = require('nodemailer');
+var EmailTemplate = require('email-templates').EmailTemplate;
+const Email = require('email-templates');
+
 // let transporter = nodemailer.createTransport(transport[, defaults]);
 
 
@@ -96,6 +99,7 @@ router.post('/', (req, res) => {
       orderDoc.update({oid: oid});
 
       // TODO send emails
+
 
       console.log('Finished saving new order:', oid);
       return res.status(200).json({
@@ -273,6 +277,81 @@ router.get('/getVendorOrders', (req, res) => {
 
 
  });
+
+router.get('/testEmail', (req, res) => {
+  // Generate SMTP service account from ethereal.email
+  let transport = nodemailer.createTestAccount((err, account) => {
+    if (err) {
+      console.error('Failed to create a testing account. ' + err.message);
+      return process.exit(1);
+    }
+
+    // console.log('Credentials obtained, sending message...');
+
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass
+      }
+    });
+
+    // Message object
+    /*
+    let message = {
+      from: 'Sender Name <sender@example.com>',
+      to: 'Recipient <recipient@example.com>',
+      subject: 'Nodemailer is unicode friendly ✔',
+      text: 'Hello to myself!',
+      html: '<p><b>Hello</b> to myself!</p>'
+    };
+    */
+
+    /*
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.log('Error occurred. ' + err.message);
+        return process.exit(1);
+      }
+
+      console.log('Message sent: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+    */
+  });
+
+  const testEmail = new Email({
+    message: {
+      from: 'test@test.com',
+      subject: 'Test Subject',
+      to: 'test@test.com'
+    },
+    send: false,
+    // transport: transport
+    transport: {
+      jsonTransport: true
+    }
+  });
+
+  testEmail.send({
+    template: 'receipt',
+    locals: {
+      location: 'Test club location here.'
+    }
+  })
+  .then(() => {
+    console.log('Finished Sending Email.');
+  })
+  .catch(console.error);
+
+
+  return res.sendStatus(200);
+
+});
 
 
 module.exports = router;
