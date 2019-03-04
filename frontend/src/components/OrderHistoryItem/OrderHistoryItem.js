240c2c16f0ad5ecdf4cb1bdf1eddc4b2d49429e4
ahong14 <ahong@ucdavis.edu>
2019-03-03 22:53:41 -0800

    added missing module for card in order history

--- frontend/src/components/OrderHistoryItem/OrderHistoryItem.js
+++ frontend/src/components/OrderHistoryItem/OrderHistoryItem.js
@@ -5,6 +5,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 class OrderHistoryItem extends Component {
   render() {
