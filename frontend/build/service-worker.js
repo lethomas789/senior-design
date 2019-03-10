d41fdfacd0393ba8c9825bf55932f9e3795505aa
ahong14 <ahong@ucdavis.edu>
2019-03-09 17:08:24 -0800

    allow user to show size options on apparel select, basic version of upload image to firestore, working on mapping pid

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ed2054e6c203297bccdc87e199995ca.js"
+  "/precache-manifest.7c223b39c5da01824f10ddb178d5ad5e.js"
 );
 
 workbox.clientsClaim();
--- frontend/src/components/AddProduct/AddProduct.js
+++ frontend/src/components/AddProduct/AddProduct.js
@@ -11,6 +11,25 @@ import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+import FormLabel from '@material-ui/core/FormLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import firebase from 'firebase';
+import FileUploader from 'react-firebase-file-uploader';
+import firebaseConfig from '../../config/ecs193-ecommerce-firebase-adminsdk-7iy3n-f581d24562.json';
+
+const config = {
+  apiKey: firebaseConfig.private_key,
+  authDomain: "ecs193-ecommerce.firebaseapp.com",
+  databaseURL: "https://ecs193-ecommerce.firebaseio.com",
+  storageBucket: "ecs193-ecommerce.appspot.com"
+};
+
+const imageLocation = "images/vendors/";
+
+firebase.initializeApp(config);
+
 
 class AddProduct extends Component {
   constructor(props){
@@ -19,30 +38,121 @@ class AddProduct extends Component {
       productName: '',
       productInfo: '',
       productPrice: '',
-      stock: ''
+      stock: '',
+      productID: '',
+      isApparel: false,
+      small: 0,
+      medium: 0,
+      large: 0,
+      xsmall: 0,
+      xlarge: 0,
+      apparelCSS: "hideApparelSizes",
+      images: []
     }
     this.addProduct = this.addProduct.bind(this);
+    this.handleUploadSuccess = this.handleUploadSuccess.bind(this);
+    // this.handleFileChange = this.handleFileChange.bind(this);
+    this.uploadFiles = this.uploadFiles.bind(this);
   }
 
+  //upload success
+  handleUploadSuccess = filename => {
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      // .getDownloadURL()
+      // .then(url => this.setState({ avatarURL: url }));
+  };
+
+  //change number of files to upload
+  handleFileChange = (event) => {
+    const { target: { files } } = event;
+    const filesToStore = [];
+    console.log(files);
+    console.log(files[0]);
+
+    filesToStore.push(files[0]);
+
+    //generate vid to match product with image
+    let randomText = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 20; i++) {
+      randomText += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    // files[0].forEach(file => {
+    //   filesToStore.push(file);
+    // });
+
+    this.setState({
+      images: filesToStore,
+      productID: randomText
+    });
+  };
+
+  //upload images to database
+  uploadFiles(){
+    const files = this.state.images;
+    files.forEach(file => {
+      this.fileUploader.startUpload(file);
+    });
+  }
+
+  //add product that is an apparel type
   //add product to vendor's collection in database
-  addProduct(){
-    const apiURL = "/api/adminProducts/addNewProduct";
-    axios.post(apiURL, {
-      params:{
-        vid: this.props.vid,
-        user: this.props.user,
-        productInfo: this.state.productInfo,
-        productName: this.state.productName,
-        productPrice: this.state.productPrice,
-        stock: this.state.stock
-      }
-    })
-    .then(res => {
-      alert(res.data.message);
-    })
-    .catch(err => {
-      alert(err);
-    })
+  addProduct(){ 
+    if(this.state.apparel === false){
+      const apiURL = "/api/adminProducts/addNewProduct";
+      axios.post(apiURL, {
+        params:{
+          vid: this.props.vid,
+          user: this.props.user,
+          productInfo: this.state.productInfo,
+          productName: this.state.productName,
+          productPrice: this.state.productPrice,
+          pid: this.state.productID,
+          stock: this.state.stock,
+          apparel: this.state.apparel,
+          s_stock: this.state.small,
+          m_stock: this.state.medium,
+          l_stock: this.state.large,
+          xs_stock: this.state.xsmall,
+          xl_stock: this.state.xlarge
+        }
+      })
+      .then(res => {
+        alert(res.data.message);
+        this.uploadFiles();
+      })
+      .catch(err => {
+        alert(err);
+      })
+    }
+
+    //if the item is not an apparel
+    else{
+      const apiURL = "/api/adminProducts/addNewProduct";
+      axios.post(apiURL, {
+        params:{
+          vid: this.props.vid,
+          user: this.props.user,
+          productInfo: this.state.productInfo,
+          productName: this.state.productName,
+          productPrice: this.state.productPrice,
+          stock: this.state.stock,
+          pid: this.state.productID
+        }
+      })
+      .then(res => {
+        alert(res.data.message);
+        this.uploadFiles();
+      })
+      .catch(err => {
+        alert(err);
+      })
+    }
   }
 
   render() {
@@ -80,6 +190,80 @@ class AddProduct extends Component {
                 required="true"
                 onChange={(event) => this.setState({ stock: event.target.value })}
               />
+            </div>
+
+            <FileUploader accept="image/*" onChange = {this.handleFileChange}
+              storageRef =  {firebase.storage().ref(imageLocation + this.state.productID)} ref = {instance => { this.fileUploader = instance; } }
+              multiple 
+            />
+
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Product Type </FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="gender2"
+                value={this.state.value}
+                onChange={this.handleChange}
+              >
+                <FormControlLabel
+                  control={<Radio color="primary" />}
+                  value = "item"
+                  label="Item"
+                  labelPlacement="start"
+                  onChange={() => this.setState({ isApparel: false, apparelCSS: 'hideApparelSizes'})}
+                />
+
+                <FormControlLabel
+                  control={<Radio color="primary" />}
+                  value = "apparel"
+                  label="Apparel"
+                  labelPlacement="start"
+                  onChange={() => this.setState({ isApparel: true, apparelCSS: 'showApparelSizes'})}
+                />
+              </RadioGroup>
+            </FormControl>
+
+            {/* add stock for apparel sizes */}
+            <div className = {this.state.apparelCSS}>
+
+              <div className = "textForm" id="row">
+                <TextField
+                  label="Small Stock"
+                  required="false"
+                  onChange={(event) => this.setState({ small: event.target.value })}
+                />
+              </div>
+
+              <div className = "textForm" id="row">
+                <TextField
+                  label="Medium Stock"
+                  required="false"
+                  onChange={(event) => this.setState({ medium: event.target.value })}
+                />
+              </div>
+
+              <div className = "textForm" id="row">
+                <TextField
+                  label="Large Stock"
+                  required="false"
+                  onChange={(event) => this.setState({ large: event.target.value })}
+                />
+              </div>
+              <div className = "textForm" id="row">
+                <TextField
+                  label="X-Small Stock"
+                  required="false"
+                  onChange={(event) => this.setState({ xsmall: event.target.value })}
+                />
+              </div>
+
+              <div className = "textForm" id="row">
+                <TextField
+                  label="X-Large Stock"
+                  required="false"
+                  onChange={(event) => this.setState({ xlarge: event.target.value })}
+                />
+              </div>
             </div>
 
             <Button variant = "contained" color = "primary" onClick = {this.addProduct}> Add Product  </Button>
