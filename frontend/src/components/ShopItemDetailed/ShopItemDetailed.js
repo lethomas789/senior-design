21df21fbceddb8fd7c2493f25e6d7a9907e65acc
ahong14 <ahong@ucdavis.edu>
2019-03-11 00:43:02 -0700

    working on distinguishing between regular item and apparel for detailed view

--- frontend/src/components/ShopItemDetailed/ShopItemDetailed.js
+++ frontend/src/components/ShopItemDetailed/ShopItemDetailed.js
@@ -15,7 +15,8 @@ class ShopItemDetailed extends Component {
       productPrice: '',
       amtPurchased: 1,
       vid: '',
-      productStock: ''
+      productStock: '',
+      isApparel: false
     };
     this.addItem = this.addItem.bind(this);
     this.addQuantity = this.addQuantity.bind(this);
@@ -44,7 +45,13 @@ class ShopItemDetailed extends Component {
           pid: this.props.pid,
           amtPurchased: this.state.amtPurchased,
           vendorID: this.state.vid,
-          image: this.state.imageLink
+          image: this.state.imageLink,
+          isApparel: this.state.isApparel,
+          s_stock: 0,
+          m_stock: 0,
+          l_stock: 0,
+          xs_stock: 0,
+          xl_stock: 0
         }
       })
       .then(res => {
@@ -109,14 +116,33 @@ class ShopItemDetailed extends Component {
       //if successfully got product info, update component
       if(res.data.success === true){
         //update state of component
-        this.setState({
-          productInfo: res.data.product.productInfo,
-          productName: res.data.product.productName,
-          productPrice: res.data.product.productPrice,
-          imageLink: res.data.product.productPicture,
-          productStock: res.data.product.productStock,
-          vid: res.data.product.vid
-        })
+        console.log("checking response for is apparel ", res.data.product);
+        if(res.data.product.isApparel === true){
+          this.setState({
+            productInfo: res.data.product.productInfo,
+            productName: res.data.product.productName,
+            productPrice: res.data.product.productPrice,
+            imageLink: res.data.product.productPicture,
+            productStock: res.data.product.productStock,
+            vid: res.data.product.vid,
+            isApparel: true,
+            s_stock: res.data.product.s_stock,
+            m_stock: res.data.product.m_stock,
+            l_stock: res.data.product.l_stock,
+            xs_stock: res.data.product.xs_stock,
+            xl_stock: res.data.product.xl_stock
+          })
+        }
+        else{
+          this.setState({
+            productInfo: res.data.product.productInfo,
+            productName: res.data.product.productName,
+            productPrice: res.data.product.productPrice,
+            imageLink: res.data.product.productPicture,
+            productStock: res.data.product.productStock,
+            vid: res.data.product.vid,
+          })
+        }
       }
       else{
         alert(res.data.message);
@@ -128,35 +154,74 @@ class ShopItemDetailed extends Component {
   }
 
   render() {
-    return (
-      <div className = "itemDetailed">        
-        <h3> {this.state.productName} </h3>
-        <div className = "itemInfo">
-          <div id = "imageContainer">
-            <img id = "detailedImage" src = {this.state.imageLink[0]}/>
-          </div>
-
-          <div id = "itemDescriptions">
-            <p> <strong> Price: </strong> ${this.state.productPrice} </p>
-            <p> <strong> Description:</strong> {this.state.productInfo} </p>
-            <p> <strong> Stock:</strong> {this.state.productStock} </p>
-            <Button size="small" color="primary" onClick = {this.addItem}>
-              Add To Cart
-            </Button>
-
-            <Button id = "test" onClick = {this.removeQuantity}>
-              -
-            </Button>
-                
-            {this.state.amtPurchased}
-                
-            <Button id = "test" onClick = {this.addQuantity}>
-              +
-            </Button>
+    if(this.state.isApparel === false){
+      return (
+        <div className = "itemDetailed">        
+          <h3> {this.state.productName} </h3>
+          <div className = "itemInfo">
+            <div id = "imageContainer">
+              <img id = "detailedImage" src = {this.state.imageLink[0]}/>
+            </div>
+  
+            <div id = "itemDescriptions">
+              <p> <strong> Price: </strong> ${this.state.productPrice} </p>
+              <p> <strong> Description:</strong> {this.state.productInfo} </p>
+              <p> <strong> Stock:</strong> {this.state.productStock} </p>
+              <Button size="small" color="primary" onClick = {this.addItem}>
+                Add To Cart
+              </Button>
+  
+              <Button id = "test" onClick = {this.removeQuantity}>
+                -
+              </Button>
+                  
+              {this.state.amtPurchased}
+                  
+              <Button id = "test" onClick = {this.addQuantity}>
+                +
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
+
+    else{
+      return(
+        <div className = "itemDetailed">        
+          <h3> {this.state.productName} </h3>
+          <div className = "itemInfo">
+            <div id = "imageContainer">
+              <img id = "detailedImage" src = {this.state.imageLink[0]}/>
+            </div>
+  
+            <div id = "itemDescriptions">
+              <p> <strong> Price: </strong> ${this.state.productPrice} </p>
+              <p> <strong> Description:</strong> {this.state.productInfo} </p>
+              <p> <strong> Stock:</strong> {this.state.productStock} </p>
+              <p> <strong> Small Stock: </strong> {this.state.s_stock} </p>
+              <p> <strong> Medium Stock:</strong> {this.state.m_stock} </p>
+              <p> <strong> Large Stock:</strong> {this.state.l_stock} </p>
+              <p> <strong> X-Small Stock: </strong> {this.state.xs_stock} </p>
+              <p> <strong> X-Large Stock:</strong> {this.state.xl_stock} </p>
+              <Button size="small" color="primary" onClick = {this.addItem}>
+                Add To Cart
+              </Button>
+  
+              <Button id = "test" onClick = {this.removeQuantity}>
+                -
+              </Button>
+                  
+              {this.state.amtPurchased}
+                  
+              <Button id = "test" onClick = {this.addQuantity}>
+                +
+              </Button>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
