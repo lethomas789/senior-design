d4f3a01503c6b607aed6668a0954c4f8a920bd08
Claudia Chen <home@campus-003-078.ucdavis.edu>
2019-04-03 13:47:00 -0700

    redoing Home page. Changed main image. Not enough images for caraousel so temporarily disabled. Trying to add bottom images to respect grid

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -14,16 +14,24 @@ export default class Home extends Component {
       <div id = "homeContainer">
         <Grid container direction="column" justify="center"alignContent = "center" alignItems="center">
           <h1> Welcome to ECS193 ECommerce! </h1>
+          <div className = "hero-image">
+          <img src={require('../../images/wics2.png')} width="100%" />
+          </div>
           <p>This is a ecommerce website for UC Davis's clubs. 
-          Here we make merchandise available from all different clubs of Davis.</p>
+          Here we make merchandise available from all different clubs of Davis.
+          </p>
+          <div className = "bottom-tabs">
+          <img src={require('../../images/cows.jpg')} width="100%" />
+          </div>
+         
 
-          <div id = "carouselContainer">
+          {/* <div id = "carouselContainer">
             <AliceCarousel autoPlay = {true} autoPlayInterval = {2000} buttonsDisabled = {true}>
                 <img className = "slideImage" src = {image1} />
                 <img className = "slideImage" src = {image2} />
                 <img className = "slideImage" src = {image3} />
             </AliceCarousel>
-          </div>
+          </div> */}
   
         </Grid>
       </div>
