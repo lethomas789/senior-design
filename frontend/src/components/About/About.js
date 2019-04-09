318f579a4a67ccd4acbe945144614874bd6081a4
Claudia Chen <home@campus-037-062.ucdavis.edu>
2019-04-08 17:21:33 -0700

    about us text section completed

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -8,6 +8,20 @@ export default class Home extends Component {
       <div>
         <Grid container direction="column" justify="center"alignContent = "center" alignItems="center">
           <h1 id = "aboutHeader"> About Us </h1>
+          <p>We are a group of Computer Science seniors and this website is our senior design final project.
+            From a technical standpoint, our goal was to make a site with secure end to end transactions using
+            modern technologies such as React, Node, Firebase, and Google Cloud Services. We utilize https protocols 
+            and the PayPal API to accomplish this. We aimed to create user and admin accounts with different privileges
+            while utilizing Oauth 2.0 to enhance userability. 
+          </p>
+
+          <p>The goals of our class is to respond to an open-ended interdisciplinary computer-science related design problem,
+            develop a precise problem statement, propose a design that solves the problem, implement a prototype design, and validate
+            the design and report on the results. We learn the complete process of a computational problem solving by 
+            experiencing the complete process of creating an initial design, implementing it, and reporting the results. 
+            We learn to function in a team-oriented problem-solving environment, and learn the issues that are involved
+            with the implementation process of large-scale.
+          </p>
         </Grid>
       </div>
     );
