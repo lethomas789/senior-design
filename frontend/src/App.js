1213f26ca0b3d46d41d9960582c217be0870a294
Thomas Le <thele@ucdavis.edu>
2019-04-12 15:27:39 -0700

    Start on footer and minor ShopView change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import './App.css';
-import About from './components/About/About';
-import Signup from './components/Signup/Signup';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import ButtonAppBar from './components/ButtonAppBar/ButtonAppBar';
-import Shop from './components/Shop/Shop';
-import Cart from './components/Cart/Cart';
-import VendorView from './components/VendorView/VendorView';
-import VendorSignup from './components/VendorSignup/VendorSignup';
-import EditClubInfo from './components/EditClubInfo/EditClubInfo';
-import AddProduct from './components/AddProduct/AddProduct';
-import OrderHistory from './components/OrderHistory/OrderHistory';
-import ShopItemDetailed from './components/ShopItemDetailed/ShopItemDetailed';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Signup from "./components/Signup/Signup";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import ButtonAppBar from "./components/ButtonAppBar/ButtonAppBar";
+import Shop from "./components/Shop/Shop";
+import Cart from "./components/Cart/Cart";
+import VendorView from "./components/VendorView/VendorView";
+import VendorSignup from "./components/VendorSignup/VendorSignup";
+import EditClubInfo from "./components/EditClubInfo/EditClubInfo";
+import AddProduct from "./components/AddProduct/AddProduct";
+import OrderHistory from "./components/OrderHistory/OrderHistory";
+import ShopItemDetailed from "./components/ShopItemDetailed/ShopItemDetailed";
+import Footer from "./components/Footer/Footer";
 
-
-import { createBrowserHistory } from 'history';
-import AboutClub from './components/AboutClub/AboutClub';
+import { createBrowserHistory } from "history";
+import AboutClub from "./components/AboutClub/AboutClub";
 const history = createBrowserHistory();
 
 class App extends Component {
@@ -26,19 +26,20 @@ class App extends Component {
       <Router>
         <div>
           <ButtonAppBar />
-          <Route exact path = "/" component = {Home} />
-          <Route exact path = "/about" component = {About} />
-          <Route exact path = "/shop" component = {Shop}/>
-          <Route exact path = "/signup" component = {Signup} />
-          <Route exact path = "/login" component = {Login} /> 
-          <Route exact path = "/cart" component = {Cart}/>
-          <Route path = "/vendorProducts/:vid" component = {VendorView}/>
-          <Route exact path = "/abcdefg/vendorSignup" component = {VendorSignup}/>
-          <Route exact path = "/editClubInfo" component = {EditClubInfo}/>
-          <Route exact path = "/addProduct" component = {AddProduct}/>
-          <Route exact path = "/orderHistory" component = {OrderHistory}/>
-          <Route path = "/itemDetails/:vid/:pid" component = {ShopItemDetailed}/>
-          <Route path="/aboutClub/:vid" component = {AboutClub}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/cart" component={Cart} />
+          <Route path="/vendorProducts/:vid" component={VendorView} />
+          <Route exact path="/abcdefg/vendorSignup" component={VendorSignup} />
+          <Route exact path="/editClubInfo" component={EditClubInfo} />
+          <Route exact path="/addProduct" component={AddProduct} />
+          <Route exact path="/orderHistory" component={OrderHistory} />
+          <Route path="/itemDetails/:vid/:pid" component={ShopItemDetailed} />
+          <Route path="/aboutClub/:vid" component={AboutClub} />
+          <Footer />
         </div>
       </Router>
     );
--- frontend/src/components/ShopView/ShopView.js
+++ frontend/src/components/ShopView/ShopView.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './ShopView.css';
-import ShopItem from '../ShopItem/ShopItem';
-import Grid from '@material-ui/core/Grid';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import actions from '../../store/actions';
-import { banner } from '../../images/generic_club.jpg';
+import React, { Component } from "react";
+import "./ShopView.css";
+import ShopItem from "../ShopItem/ShopItem";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import { connect } from "react-redux";
+import actions from "../../store/actions";
+import { banner } from "../../images/generic_club.jpg";
 
 class ShopView extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   //get products from server after mounting to screen
-  componentDidMount(){
+  componentDidMount() {
     //const apiURL = "http://localhost:4000/api/getAllProducts";
     const apiURL = "/api/getAllProducts";
     //get all products from server
     //update state of view to obtain items
-    axios.get(apiURL)
+    axios
+      .get(apiURL)
       .then(res => {
         //update product state in redux store
         console.log(res.data);
@@ -26,47 +27,75 @@ class ShopView extends Component {
 
         //after getting products, get list of vendors
         const vendorsURL = "/api/getVendorInfo";
-        axios.get(vendorsURL)
+        axios
+          .get(vendorsURL)
           .then(res => {
             console.log(res.data);
             this.props.updateVendors(res.data.vendors);
           })
           .catch(err => {
             alert("error getting vendors");
-          })
+          });
       })
       .catch(err => {
         alert("Server error retrieving items");
-      })
+      });
   }
 
   render() {
     const items = this.props.products.map(result => {
-      return <ShopItem key = {result.pid} imageSrc = {result.productPicture[0]} vendorID = {result.vid} pid = {result.pid} productName = {result.productName} productPrice = {result.productPrice} stock = {result.stock} productInfo = {result.productInfo} />
+      return (
+        <ShopItem
+          key={result.pid}
+          imageSrc={result.productPicture[0]}
+          vendorID={result.vid}
+          pid={result.pid}
+          productName={result.productName}
+          productPrice={result.productPrice}
+          stock={result.stock}
+          productInfo={result.productInfo}
+        />
+      );
     });
 
     return (
-      <div className = "grow">
-         <Grid container direction = "column" display= "flex"  justifyContent= "center" alignItems="center" height= "100vh">
-        {/* <Grid container direction="column" justify="center"alignContent = "center" alignItems="center"> */}
-          <h1> Shop </h1>    
-          <div className = "container">     
-          <div className = "hero-image">
-          <img src={require('../../images/more_treats.jpg')} width="100%" height="100%"/>
+      <div className="grow">
+        <Grid
+          container
+          direction="column"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          {/* <Grid container direction="column" justify="center"alignContent = "center" alignItems="center"> */}
+          <h1> Shop </h1>
+          <div className="shop-hero-image-container">
+            <div className="hero-image">
+              <img
+                src={require("../../images/more_treats.jpg")}
+                width="100%"
+                height="100%"
+              />
+            </div>
+            <div class="hero-text">UCD CLUBS</div>
           </div>
-          <div class="hero-text">
-          UCD CLUBS
-          </div>
-          </div> 
         </Grid>
-        
-        <Grid container direction = "row" display= "flex"  justifyContent= "center" alignItems="center" height= "100vh" justify-xs-space-evenly>
 
-        {/* <Grid container spacing={24} direction="row" justify="flex-start" alignItems="center" justify-xs-space-evenly> */}
+        <Grid
+          container
+          direction="row"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+          justify-xs-space-evenly
+        >
+          {/* <Grid container spacing={24} direction="row" justify="flex-start" alignItems="center" justify-xs-space-evenly> */}
           {items}
         </Grid>
       </div>
-    )
+    );
   }
 }
 
@@ -75,25 +104,30 @@ class ShopView extends Component {
 //dispatch action to reducer
 //update items from server to become state of store
 const mapDispatchToProps = dispatch => {
-  return{
-      updateProducts: (products) => dispatch({
-          type: actions.GET_PRODUCTS,
-          items: products
+  return {
+    updateProducts: products =>
+      dispatch({
+        type: actions.GET_PRODUCTS,
+        items: products
       }),
 
-      updateVendors: (currentVendors) => dispatch({
+    updateVendors: currentVendors =>
+      dispatch({
         type: actions.GET_VENDORS,
         vendors: currentVendors
       })
-  }
-}
+  };
+};
 
 //get items from products state of store
 //obtain state from store as props for component
 const mapStateToProps = state => {
-  return{
-      products: state.getAllItems.products
-  }
-}
+  return {
+    products: state.getAllItems.products
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopView);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShopView);
