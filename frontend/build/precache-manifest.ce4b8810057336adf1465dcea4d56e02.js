8565cb45d893adc0518d42b408302eb6c13c0d22
ahong14 <ahong@ucdavis.edu>
2019-04-15 13:52:15 -0700

    fixing merge conflict

---
+++ frontend/build/precache-manifest.ce4b8810057336adf1465dcea4d56e02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeeb273de5f1a695779e",
+    "url": "/static/css/main.1af860d2.chunk.css"
+  },
+  {
+    "revision": "aeeb273de5f1a695779e",
+    "url": "/static/js/main.aeeb273d.chunk.js"
+  },
+  {
+    "revision": "e6c8fb058c1663c7e0b4",
+    "url": "/static/css/1.484ba7cb.chunk.css"
+  },
+  {
+    "revision": "e6c8fb058c1663c7e0b4",
+    "url": "/static/js/1.e6c8fb05.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1adfb96a885e9136f36b3ac77ec4c5f8",
+    "url": "/static/media/generic_club.1adfb96a.jpg"
+  },
+  {
+    "revision": "aa9094e40b1936f88538aa592d4c36a8",
+    "url": "/static/media/dog1.aa9094e4.jpg"
+  },
+  {
+    "revision": "303269b8c6650c229994ca65e4d400e3",
+    "url": "/static/media/dog2.303269b8.jpg"
+  },
+  {
+    "revision": "751aa1615caa0e77521fe3563fe23048",
+    "url": "/static/media/dog3.751aa161.jpg"
+  },
+  {
+    "revision": "f90d2347b4c2f9f799463c5d267c67de",
+    "url": "/index.html"
+  }
+];
