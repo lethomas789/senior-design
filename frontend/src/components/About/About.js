4a1cb73c817402044ce7984da0225f81018302fb
Claudia Chen <home@Claudias-MacBook-Air.local>
2019-04-24 00:15:59 -0700

    basic layout up

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -14,9 +14,11 @@ export default class About extends Component {
           IMAGE TEXT HERE
         </div>
         <section className="about-info">
+        <div className = "about-info-text">
             We are a group of Computer Science seniors and this website is our senior design final project.
             From a technical standpoint, our goal was to make a site with secure end to end transactions using
             modern technologies such as React, Node, Firebase, and Google Cloud Services. 
+            </div>
         </section>
         <section className="about-link-box about-link-box-1">BOX 1</section>
         <section className="about-link-box about-link-box-2">BOX 2</section>
