98c4bd1533c52966b0dc2644120a3be9558df91c
Thomas Le <thele@ucdavis.edu>
2019-04-26 13:16:07 -0700

    Start on recover password styling

--- frontend/src/components/InputRecoveryPassword/InputRecoveryPassword.js
+++ frontend/src/components/InputRecoveryPassword/InputRecoveryPassword.js
@@ -1,80 +1,93 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 class InputRecoveryPassword extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      email:'',
-      password: '',
-      confirmPassword: ''
-    }
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
   }
 
   //function to handle email change on input
-  handleEmailField = (event) => {
+  handleEmailField = event => {
     this.setState({
       email: event.target.value
-    })
-  }
+    });
+  };
 
   //change password on input
-  handlePasswordField = (event) => {
+  handlePasswordField = event => {
     this.setState({
       password: event.target.value
-    })
-  }
+    });
+  };
 
   //change confirm password on input change
-  handleConfirmPasswordField = (event) => {
+  handleConfirmPasswordField = event => {
     this.setState({
       confirmPassword: event.target.value
-    })
-  }
+    });
+  };
 
   //recoverPassword
   recoverPassword = () => {
-    if(this.state.password != this.state.confirmPassword){
+    if (this.state.password != this.state.confirmPassword) {
       alert("Passwords do not match");
       return;
     }
 
-    const apiURL = '/api/setNewPassword';
-    axios.patch(apiURL, {
-      params:{
-        email: this.state.email,
-        password: this.state.password,
-        confirmPassword: this.state.confirmPassword
-      }
-    })
-    .then(res => {
-      if(res.data.success === true){
-        alert(res.data.message);
-        this.props.history.push('/login');
-      }
-    })
-    .catch(err => {
-      alert(err);
-    })
-  }
+    const apiURL = "/api/setNewPassword";
+    axios
+      .patch(apiURL, {
+        params: {
+          email: this.state.email,
+          password: this.state.password,
+          confirmPassword: this.state.confirmPassword
+        }
+      })
+      .then(res => {
+        if (res.data.success === true) {
+          alert(res.data.message);
+          this.props.history.push("/login");
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  };
 
   render() {
     return (
       <div>
-        <div id = "enterEmailForm">
-          <h1 id = "passwordTitle"> Password Recovery </h1>
+        <div id="enterEmailForm">
+          <h1 id="passwordTitle"> Password Recovery </h1>
           <h3> Enter email and new password: </h3>
-          <form id = "emailForm">
-            Email: <input onChange = {this.handleEmailField}/> 
-            Password: <input label = "Password" type = "password" onChange = {this.handlePasswordField}/> 
-            Confirm Password: <input label = "Confirm Password" type = "password" onChange = {this.handleConfirmPasswordField}/> 
+          <form id="emailForm">
+            Email: <input onChange={this.handleEmailField} />
+            Password:{" "}
+            <input
+              label="Password"
+              type="password"
+              onChange={this.handlePasswordField}
+            />
+            Confirm Password:{" "}
+            <input
+              label="Confirm Password"
+              type="password"
+              onChange={this.handleConfirmPasswordField}
+            />
           </form>
-          <Button color = "primary" onClick = {this.recoverPassword}> Recover Password </Button>
+          <Button color="primary" onClick={this.recoverPassword}>
+            {" "}
+            Recover Password{" "}
+          </Button>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- frontend/src/components/RecoverPassword/RecoverPassword.js
+++ frontend/src/components/RecoverPassword/RecoverPassword.js
@@ -1,64 +1,79 @@
-import React, { Component } from 'react';
-import './RecoverPassword.css';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import "./RecoverPassword.css";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
 class RecoverPassword extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
-      confirmPassword: ''
-    }
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
   }
 
   //function to handle email change on input
-  handleEmailField = (event) => {
+  handleEmailField = event => {
     this.setState({
       email: event.target.value
-    })
-  }
+    });
+  };
 
   //function called when clicking recover password
   sendRecoverEmail = () => {
     //get request to send email with password
-    const apiURL = '/api/recoverPassword';
+    const apiURL = "/api/recoverPassword";
     console.log("this is the input of the user's email", this.state.email);
     alert("Sending email to user, check your inbox for verification link!");
-    axios.patch(apiURL, {
-      params:{
-        email: this.state.email
-      }
-    })
-    .then(res => {
-      if(res.data.success === true){
-        alert(res.data.message);
-      }
-
-      else{
-        alert("no password for user that signed in with google");
-      }
-    })
-    .catch(err => {
-      alert(err);
-    })
-  }
+    axios
+      .patch(apiURL, {
+        params: {
+          email: this.state.email
+        }
+      })
+      .then(res => {
+        if (res.data.success === true) {
+          alert(res.data.message);
+        } else {
+          alert("no password for user that signed in with google");
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  };
 
   render() {
     return (
-      <div>
-        <div id = "enterEmailForm">
-          <h1 id = "passwordTitle"> Password Recovery </h1>
-          <h3> Enter email, link will be sent to proceed with password reset </h3>
-          <form id = "emailForm">
-            Email: <input onChange = {this.handleEmailField}/> 
+      <div id="enter-email-container">
+        <Paper className="password-paper-container">
+          <h2> Password Recovery </h2>
+          <div>
+            Please enter your email, and we will send you a password reset link.
+          </div>
+          <form>
+            <TextField
+              label="Email"
+              required="true"
+              onChange={this.handleEmailField}
+            />
           </form>
-          <Button color = "primary" onClick = {this.sendRecoverEmail}> Recover Password </Button>
-        </div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.sendRecoverEmail}
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            Recover Password
+          </Button>
+        </Paper>
       </div>
-    )
+    );
   }
 }
 
