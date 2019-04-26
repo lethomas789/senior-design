f94c6acb4d8d95641b766f6e129737890a876668
Claudia Chen <home@campus-053-007.ucdavis.edu>
2019-04-26 13:49:53 -0700

    fixed the footer running into order history

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -11,7 +11,7 @@ export default class About extends Component {
           {/* <div>4 students trying to inspire</div> */}
         </header>
         <div className="about-main-image">
-          IMAGE TEXT HERE
+         
         </div>
         <section className="about-info">
         <div className = "about-info-text">
