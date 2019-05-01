14e69881d8c4af8331f80cd3176b7023688d893c
Claudia Chen <home@campus-040-173.ucdavis.edu>
2019-05-01 11:32:37 -0700

    refactoring

--- frontend/src/components/AccountInfo/AccountInfo.js
+++ frontend/src/components/AccountInfo/AccountInfo.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './AccountInfo.css';
+import Paper from "@material-ui/core/Paper";
 
 class AccountInfo extends Component {
   render() {
     return (
-      <div>
+      <div className ="account-info-header">
+      <div className = "accountForms">
+      <Paper className="paper-account-Container">
         <h1> Account Info Here:  </h1>
         <h1> First Name:  </h1>
         <h1> Last Name:  </h1>
         <h1> Orders: </h1>
         <h1> Other Info </h1>
+        </Paper>
+        </div>
       </div>
     )
   }
