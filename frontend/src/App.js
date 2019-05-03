50fd58074fdf329e991b7968bff35d84bbb08ac6
Thomas Le <thele@ucdavis.edu>
2019-05-03 14:58:57 -0700

    Update with prettier styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Signup from "./components/Signup/Signup";
@@ -24,21 +23,20 @@ import CartView from "./components/CartView/CartView";
 import EmailConfirmation from "./components/EmailConfirmation/EmailConfirmation";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
-
 import { createBrowserHistory } from "history";
 import AboutClub from "./components/AboutClub/AboutClub";
 import Clubs from "./components/Clubs/Clubs";
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const history = createBrowserHistory();
 
 class App extends Component {
   notificationDOMRef = React.createRef();
 
-  addNotification = ({ title, message, type, duration=2500 }) => {
+  addNotification = ({ title, message, type, duration = 2500 }) => {
     this.notificationDOMRef.current.addNotification({
       title: title,
       message: message,
@@ -56,116 +54,100 @@ class App extends Component {
     return (
       <Router>
         <ScrollToTop>
+          <div>
+            <ButtonAppBar notifier={this.addNotification} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/shop" component={Shop} />
+            <Route
+              exact
+              path="/signup"
+              render={() => <Signup notifier={this.addNotification} />}
+            />
 
-        <div>
-          <ButtonAppBar notifier={this.addNotification}/>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/shop" component={Shop} />
-          <Route
-            exact
-            path="/signup"
-            render={() => <Signup notifier={this.addNotification} />}
-          />
+            <Route
+              exact
+              path="/login"
+              render={props => (
+                <Login {...props} notifier={this.addNotification} />
+              )}
+            />
 
-          <Route
-            exact
-            path="/login"
-            render={props => (
-              <Login {...props} notifier={this.addNotification} />
-            )}
-          />
+            <Route
+              exact
+              path="/cart"
+              render={props => (
+                <CartView {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Route path="/vendorProducts/:vid" component={VendorView} />
+            <Route
+              exact
+              path="/abcdefg/vendorSignup"
+              component={VendorSignup}
+            />
+            <Route
+              exact
+              path="/editClubInfo"
+              render={props => (
+                <EditClubInfo {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Route
+              exact
+              path="/addProduct"
+              render={props => (
+                <AddProduct {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Route
+              exact
+              path="/orderHistory"
+              render={props => (
+                <OrderHistory {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Route
+              path="/itemDetails/:vid/:pid"
+              render={props => (
+                <ShopItemDetailed {...props} notifier={this.addNotification} />
+              )}
+            />
 
-          <Route
-            exact
-            path="/cart"
-            render={props => (
-              <CartView
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route path="/vendorProducts/:vid" component={VendorView} />
-          <Route exact path="/abcdefg/vendorSignup" component={VendorSignup} />
-          <Route
-            exact
-            path="/editClubInfo"
-            render={props => (
-              <EditClubInfo
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/addProduct"
-            render={props => (
-              <AddProduct
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/orderHistory"
-            render={props => (
-              <OrderHistory
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route
-            path="/itemDetails/:vid/:pid"
-            render={props => (
-              <ShopItemDetailed {...props} notifier={this.addNotification} />
-            )}
-          />
+            <Route path="/aboutClub/:vid" component={AboutClub} />
+            <Route exact path="/editItem" component={EditItemView} />
+            <Route exact path="/clubs" component={Clubs} />
+            <Route exact path="/accountInfo" component={AccountInfo} />
 
-          <Route path="/aboutClub/:vid" component={AboutClub} />
-          <Route exact path="/editItem" component={EditItemView} />
-          <Route exact path="/clubs" component={Clubs} />
-          <Route exact path="/accountInfo" component={AccountInfo} />
-
-          <Route
-            exact
-            path="/recoverPassword"
-            render={props => (
-              <RecoverPassword
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route
-            path="/inputNewPassword"
-            render={props => (
-              <InputRecoveryPassword
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Route
-            path="/emailConfirmation/:token"
-            render={props => (
-              <EmailConfirmation
-                {...props}
-                notifier={this.addNotification}
-              />
-            )}
-          />
-          <Footer />
-          <ReactNotification ref={this.notificationDOMRef} />
-        </div>
+            <Route
+              exact
+              path="/recoverPassword"
+              render={props => (
+                <RecoverPassword {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Route
+              path="/inputNewPassword"
+              render={props => (
+                <InputRecoveryPassword
+                  {...props}
+                  notifier={this.addNotification}
+                />
+              )}
+            />
+            <Route
+              path="/emailConfirmation/:token"
+              render={props => (
+                <EmailConfirmation {...props} notifier={this.addNotification} />
+              )}
+            />
+            <Footer />
+            <ReactNotification ref={this.notificationDOMRef} />
+          </div>
         </ScrollToTop>
       </Router>
     );
   }
 }
-
 
 export default App;
