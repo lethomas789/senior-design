825851eeb152c57a6424f6641c7fef582025ffc9
Thomas Le <thele@ucdavis.edu>
2019-05-08 16:53:57 -0700

    Update login routes to send token on login

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const firebase = require('firebase');
-const admin = require('firebase-admin');
+const firebase = require("firebase");
+const admin = require("firebase-admin");
 const db = admin.firestore();
-const validator = require('validator');
-const bcrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
-const jwtKey = require('../config/jwt.json');
+const validator = require("validator");
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
+const jwtKey = require("../config/jwt.json");
 
-router.post('/', (req, res) =>{
+router.post("/", (req, res) => {
   //extract email and password from request
   if (req.body.params) {
     var email = req.body.params.email.toLowerCase();
     var password = req.body.params.password;
-  }
-  else {
+  } else {
     var email = req.body.email.toLowerCase();
     var password = req.body.password;
   }
 
   //if email is invalid
-  if (validator.isEmail(email) === false){
+  if (validator.isEmail(email) === false) {
     return res.status(200).json({
       success: false,
       message: "Invalid Email"
@@ -28,7 +27,7 @@ router.post('/', (req, res) =>{
   }
 
   //if input fields were empty
-  if (email.trim() === '' || password.trim() === ''){
+  if (email.trim() === "" || password.trim() === "") {
     return res.status(200).json({
       success: false,
       message: "Empty Inputs"
@@ -37,123 +36,142 @@ router.post('/', (req, res) =>{
 
   // check to see if an account exists with given email
 
-  const userRef = db.collection('users').doc(email);
-  userRef.get().then(doc => {
-    if (!doc.exists) {
-      console.log('No such account for given email:', email);
+  const userRef = db.collection("users").doc(email);
+  userRef
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log("No such account for given email:", email);
+        return res.status(200).json({
+          success: false,
+          message: "No such account for given email: " + email
+        });
+      }
+
+      if (!doc.data().isVerified) {
+        console.log("Login denied for unverified account:", email);
+        return res.json({
+          success: false,
+          message:
+            "Please check your email to activate your account, before logging in."
+        });
+      }
+
+      bcrypt.compare(password, doc.data().password, (err, validPassword) => {
+        if (err) {
+          return res.status(200).json({
+            success: false,
+            message: "Server error comparing passwords"
+          });
+        }
+
+        let vendors = [];
+
+        // if passwords match, send JWT to authenticate login
+        if (validPassword) {
+          // if they are an admin, get their vendors
+          if (doc.data().isAdmin) {
+            // we assume they will exist in admin collection
+            db.collection("admins")
+              .doc(email)
+              .get()
+              .then(doc => {
+                if (!doc.exists) {
+                  console.log("Server error in getting admin info");
+                  return res.status(200).json({
+                    success: false,
+                    message: "Server error in getting admin info"
+                  });
+                }
+                vendors = doc.data().vendors;
+
+                // info that JWT stores; TODO include anything else for frontend
+                const payload = { email, vendors };
+
+                jwt.sign(
+                  payload,
+                  jwtKey.JWTSecret,
+                  { expiresIn: "1h" },
+                  (err, token) => {
+                    if (err) {
+                      console.log(err);
+                      return res.json({
+                        success: false,
+                        message: "Error in generating jwt."
+                      });
+                    }
+
+                    return res.status(200).json({
+                      success: true,
+                      message: "Login Successful!",
+                      token: token
+                    });
+
+                    // OLD TODO DELETE once frontend is changed
+                    // return res.status(200).json({
+                    //   success: true,
+                    //   message: "Login Successful!",
+                    //   email,
+                    //   vendors
+                    // });
+                  }
+                );
+              })
+              .catch(err => {
+                console.log("Server error in getting admin info:", err);
+                return res.status(200).json({
+                  success: false,
+                  message: "Server error in getting admin info: " + err
+                });
+              });
+          }
+
+          // else not admin, send empty array
+          else {
+            // info that JWT stores
+            const payload = { email: email };
+
+            jwt.sign(
+              payload,
+              jwtKey.JWTSecret,
+              { expiresIn: 3600 },
+              (err, token) => {
+                return res.status(200).json({
+                  success: true,
+                  message: "Login Successful!",
+                  token: "Bearer " + token,
+                  email,
+                  vendors
+                });
+              }
+            );
+          }
+        }
+
+        // if passwords don't match
+        else {
+          return res.status(200).json({
+            success: false,
+            message: "Incorrect Password"
+          });
+        }
+      });
+    })
+    .catch(err => {
+      // catch for userRef.get
+      console.log("Error in getting userRef:", err);
       return res.status(200).json({
         success: false,
-        message: 'No such account for given email: ' + email
+        message: "Error in getting userRef: " + err
       });
-    }
-
-    if (!doc.data().isVerified) {
-      console.log('Login denied for unverified account:', email);
-      return res.json({
-        success: false,
-        message: 'Please check your email to activate your account, before logging in.',
-      });
-    }
-
-    bcrypt.compare(password, doc.data().password, (err, validPassword) => {
-      if (err) {
-        return res.status(200).json({
-          success: false,
-          message: "Server error comparing passwords"
-        });
-      }
-
-      let vendors = [];
-
-      // if passwords match, send JWT to authenticate login
-      if (validPassword) {
-        // if they are an admin, get their vendors
-        if (doc.data().isAdmin) {
-          // we assume they will exist in admin collection
-          db.collection('admins').doc(email).get().then(doc => {
-            if (!doc.exists) {
-              console.log('Server error in getting admin info');
-              return res.status(200).json({
-                success: false,
-                message: 'Server error in getting admin info' 
-              });
-            }
-            vendors = doc.data().vendors;
-
-            //info that JWT stores
-            const payload = { email };
-
-            jwt.sign(payload, jwtKey.JWTSecret, { expiresIn: 3600 }, (err, token) => {
-              return res.status(200).json({
-                success: true,
-                message: "Login Successful!",
-                token: 'Bearer ' + token,
-                email,
-                vendors
-              });
-            });
-
-          })
-          .catch(err => {
-            console.log('Server error in getting admin info:', err);
-            return res.status(200).json({
-              success: false,
-              message: 'Server error in getting admin info: ' + err
-            });
-          });
-        }
-
-        // else not admin, send empty array
-        else {
-          // info that JWT stores
-          const payload = { email: email };
-
-          jwt.sign(payload, jwtKey.JWTSecret, { expiresIn: 3600 }, (err, token) => {
-            return res.status(200).json({
-              success: true,
-              message: "Login Successful!",
-              token: 'Bearer ' + token,
-              email,
-              vendors
-            });
-          });
-        }
-
-      }
-
-      // if passwords don't match
-      else {
-        return res.status(200).json({
-          success: false,
-          message: "Incorrect Password"
-        });
-      }
     });
+});
 
-  })
-  .catch(err => {  // catch for userRef.get
-    console.log('Error in getting userRef:', err);
-    return res.status(200).json({
-      success: false,
-      message: 'Error in getting userRef: ' + err
-    });
-  });
-})
-
-router.get('/googleLogin', (req, res) => {
+router.get("/googleLogin", (req, res) => {
   if (req.query.params) {
-    var {
-      email,
-      firstName,
-      lastName
-    } = req.query.params;
-  }
-  else {
-    var {
-      email,
-      firstName,
-      lastName
-    } = req.query;
+    var { email, firstName, lastName } = req.query.params;
+  } else {
+    var { email, firstName, lastName } = req.query;
   }
 
   if (!email || !firstName || !lastName) {
@@ -164,77 +182,100 @@ router.get('/googleLogin', (req, res) => {
     });
   }
 
-  const userRef = db.collection('users').doc(email);
+  const userRef = db.collection("users").doc(email);
 
-  userRef.get().then(doc => {
-    if(!doc.exists) {
-      console.log('No such account for provided email:', email);
-      return res.json({
-        success: false,
-        message: 'Sorry, no such account for this email.'
-      });
-    }
-
-    if(doc.data().isVerified === false) {
-      console.log('No such account for provided email:', email);
-      return res.json({
-        success: false,
-        message: 'Please verify your account through your email before logging in.'
-      });
-    }
-
-    if(doc.data().isAdmin) {
-      db.collection('admins').doc(email).get().then(adoc => {
-        if (!adoc.exists) {
-          console.log('Server error in getting admin info');
-          return res.status(200).json({
-            success: false,
-            message: 'Server error in getting admin info' 
-          });
-        }
-        const vendors = adoc.data().vendors;
-        console.log('vendors here', vendors);
-
-        return res.status(200).json({
-          success: true,
-          message: "Login Successful!",
-          email,
-          vendors
-        });
-      })
-      .catch(err => {
-        console.log('Server error in getting admin info:', err);
-        return res.status(200).json({
+  userRef
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log("No such account for provided email:", email);
+        return res.json({
           success: false,
-          message: 'Server error in getting admin info: ' + err
+          message: "Sorry, no such account for this email."
         });
-      });
-    }
-    else {
+      }
+
+      if (doc.data().isVerified === false) {
+        console.log("No such account for provided email:", email);
+        return res.json({
+          success: false,
+          message:
+            "Please verify your account through your email before logging in."
+        });
+      }
+
+      if (doc.data().isAdmin) {
+        db.collection("admins")
+          .doc(email)
+          .get()
+          .then(adoc => {
+            if (!adoc.exists) {
+              console.log("Server error in getting admin info");
+              return res.status(200).json({
+                success: false,
+                message: "Server error in getting admin info"
+              });
+            }
+            const vendors = adoc.data().vendors;
+            // info that JWT stores; TODO include anything else for frontend
+            const payload = { email, vendors };
+
+            jwt.sign(
+              payload,
+              jwtKey.JWTSecret,
+              { expiresIn: "1h" },
+              (err, token) => {
+                if (err) {
+                  console.log(err);
+                  return res.json({
+                    success: false,
+                    message: "Error in generating jwt."
+                  });
+                }
+
+                return res.status(200).json({
+                  success: true,
+                  message: "Login Successful!",
+                  token: token
+                });
+              }
+            );
+
+            // OLD TODO DELETE once frontend is changed
+            // return res.status(200).json({
+            //   success: true,
+            //   message: "Login Successful!",
+            //   email,
+            //   vendors
+            // });
+          })
+          .catch(err => {
+            console.log("Server error in getting admin info:", err);
+            return res.status(200).json({
+              success: false,
+              message: "Server error in getting admin info: " + err
+            });
+          });
+      } else {
+        return res.json({
+          success: true,
+          message: "Login successful.",
+          email,
+          vendors: []
+        });
+      }
+    })
+    .catch(err => {
+      console.log("Server error in google signup route:", err);
       return res.json({
-        success: true,
-        message: 'Login successful.',
-        email,
-        vendors: [],
+        success: false,
+        message: "Sorry there was a server error. Please try again later."
       });
-    }
-  })
-  .catch(err => {
-    console.log('Server error in google signup route:', err);
-    return res.json({
-      success: false,
-      message: 'Sorry there was a server error. Please try again later.'
     });
-  });
-
-
-
-
 });
 
 //extract email parameter from google oauth
-router.get('/gmail', (req,res) => {
-
+router.get("/gmail", (req, res) => {
   //database parameters are passed through query
   var query = req.query;
   var email = "";
@@ -246,27 +287,29 @@ router.get('/gmail', (req,res) => {
   email = query.email.trim();
   firstName = query.firstName.trim();
   lastName = query.lastName.trim();
-  
+
   //find email, similar logic as regular login
   //main difference is extracting login credentials using gmail params
-  const userRef = db.collection('users').doc(email);
+  const userRef = db.collection("users").doc(email);
   userRef.get().then(doc => {
     //if the gmail for the user does not exist, create a new user in firestore database with gmail as identifier
     //combo of signup + login code
-    if(!doc.exists){
+    if (!doc.exists) {
       // no matching results, create new user
       // create object to store into database
       const newUser = {
         name: {
           firstName,
-          lastName,
+          lastName
         },
         email,
         isAdmin: false
-      }
+      };
 
       //create new user in database with parameters passed from google login oauth
-      db.collection('users').doc(email).set(newUser);
+      db.collection("users")
+        .doc(email)
+        .set(newUser);
       return res.status(200).json({
         success: true,
         message: "Login Successful!",
@@ -275,33 +318,36 @@ router.get('/gmail', (req,res) => {
     }
 
     //sign in user
-    else{
-      if(doc.data().isAdmin) {
+    else {
+      if (doc.data().isAdmin) {
         // we assume they will exist in admin collection
-        db.collection('admins').doc(email).get().then(doc => {
-          if (!doc.exists) {
-            console.log('Server error in getting admin info');
+        db.collection("admins")
+          .doc(email)
+          .get()
+          .then(doc => {
+            if (!doc.exists) {
+              console.log("Server error in getting admin info");
+              return res.status(200).json({
+                success: false,
+                message: "Server error in getting admin info"
+              });
+            }
+            vendors = doc.data().vendors;
+
+            return res.status(200).json({
+              success: true,
+              message: "Login Successful!",
+              email,
+              vendors
+            });
+          })
+          .catch(err => {
+            console.log("Server error in getting admin info:", err);
             return res.status(200).json({
               success: false,
-              message: 'Server error in getting admin info' 
+              message: "Server error in getting admin info: " + err
             });
-          }
-          vendors = doc.data().vendors;
-
-          return res.status(200).json({
-            success: true,
-            message: "Login Successful!",
-            email,
-            vendors
           });
-        })
-        .catch(err => {
-          console.log('Server error in getting admin info:', err);
-          return res.status(200).json({
-            success: false,
-            message: 'Server error in getting admin info: ' + err
-          });
-        });
       }
 
       // else not admin, send empty array
@@ -315,9 +361,6 @@ router.get('/gmail', (req,res) => {
       }
     }
   });
-})
-
+});
 
 module.exports = router;
-
-
